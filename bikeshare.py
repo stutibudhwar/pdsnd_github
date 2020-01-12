8c5463704c90849f34a8be8ff96c4c27fdf5ef0d
Stuti Budhwar <stuts0699@gmail.com>
2020-01-11 16:16:08 -0800

    all field added to CITIES to enable user to get data for all 3 cities together

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-CITIES = ['chicago', 'new york city', 'washington']
+CITIES = ['chicago', 'new york city', 'washington', 'all']
 
 MONTHS = ['january', 'february', 'march', 'april', 'may', 'june', 'all']
 
